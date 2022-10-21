a4e9c9f64129d9d1c70b4620e428a0bd5f82c2d3
Bishwahang Dewan <bishwahangdewan123@gmail.com>
2022-10-21 17:12:19 +0530

    FEAT : Create CRUD functionality

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
 
 //import routes
 const blog = require('./src/routes/blog/Blog.router.js');
@@ -10,15 +16,41 @@ const app = express();
 
 const port = 3000;
 
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/travel&live')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
+
+//setup sessions and flash
+app.use(session({
+    secret:'flashblog',
+    saveUninitialized: true,
+    resave: true
+}));
+app.use(flash());
+
 //templating engine config - handlebars
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/src/views'));
-hbs.registerPartials(__dirname + '/src/views/partials', (err) => {
-  console.log(err);
-})
+hbs.registerPartials(__dirname + '/src/views/partials');
+
+//displaying flash messages
+app.use(function(req, res, next){
+    res.locals.message = req.flash();
+    next();
+});
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
 
 //static files serve config
 app.use(express.static(path.join(__dirname, 'public')))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// parse application/json
+app.use(bodyParser.json())
 
 //useRoutes
 app.use(blog);
--- src/routes/admin/Admin.router.js
+++ src/routes/admin/Admin.router.js
@@ -1,12 +1,96 @@
 const express = require('express');
 const router = express.Router();
 
+//import modals
+const Blog = require('../../modals/Blog.js');
+
+//get the admin login page
 router.get('/admin', (req, res) => {
   res.render('adminLogin');
 })
 
+//get the dashboard page
 router.get('/admin/dashboard', (req, res) => {
-  res.render('dashboard');
+
+  //get all posts
+  Blog.find({})
+    .then((data) => {
+      res.render('dashboard', { posts: data });
+    })
+    .catch((err) => console.log(err));
 })
+
+//get the create post page
+router.get('/admin/createPost', (req,res) => {
+  res.render('createPost');
+})
+
+//test flash msg
+router.get('/admin/flash', (req, res) => {
+  res.send(req.flash('message'));
+})
+
+//add a POST
+router.post('/admin/createPost', (req, res) => {
+  const blogTitle = req.body.title;
+  const blogArticle = req.body.article;
+
+  //creating the instance of the model
+  const newBlogPost = new Blog({
+    title : blogTitle,
+    article : blogArticle,
+  });
+
+  //saving in the databse
+  newBlogPost.save()
+    .then(() => {
+      console.log("Blog Post added to the database");
+      req.flash('message', 'Blog Post added to the database')
+      res.redirect('/admin/dashboard');
+    })
+    .catch((err) => console.log(err));
+
+})
+
+//get the edit post page
+router.get('/admin/editPost/:id', (req,res) => {
+
+  //get post based on id
+  Blog.findOne({
+    _id : req.params.id,
+  }).then((data) => {
+    res.render('editPost', { post: data });
+  })
+})
+
+//edit a post
+router.put('/admin/editPost/:id', (req, res) => {
+  //find the blog post and update it
+  Blog.findOne({
+    _id : req.params.id,
+  })
+  .then((data) => {
+    data.title = req.body.title;
+    data.article = req.body.article;
+
+    //save the updated data
+    data.save()
+      .then((savedData) => {
+        req.flash('message', 'Your post has been updated successfully');
+        res.redirect('/admin/dashboard');
+      })
+  })
+})
+
+//delete a post
+router.delete('/admin/deletePost/:id', (req, res) => {
+  Blog.findByIdAndDelete(req.params.id)
+    .then(() => {
+      req.flash('message', 'Your post has been deleted successfully');
+      res.redirect('/admin/dashboard');
+    })
+    .catch((err) => console.log(err));
+})
+
 
 module.exports = router;
